a5a91f93102c20193b5a11e08d8a5f3ba9442105
avocadopelvis <avotheyo23@gmail.com>
2022-10-31 23:40:14 +0530

    added report

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras import layers
 
-
+ 
 #load the json file
 with open('intents.json') as file:
     data = json.load(file)
@@ -30,7 +30,7 @@ for intent in data['intents']:
 
     if intent['tag'] not in labels:
         labels.append(intent['tag'])
-
+    
 num_classes = len(labels)
 
 lbl_encoder = LabelEncoder()
@@ -49,13 +49,33 @@ word_index = tokenizer.word_index
 sequences = tokenizer.texts_to_sequences(training_sentences)
 padded_sequences = pad_sequences(sequences, truncating = 'post', maxlen = max_len)
 
-#model1
+#model
 model = Sequential()
 model.add(Embedding(vocab_size, embedding_dim, input_length=max_len))
 model.add(GlobalAveragePooling1D())
 model.add(Dense(16, activation='relu'))
 model.add(Dense(16, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy', 
+              optimizer='adam', metrics=['accuracy']) 
+           
+
+model.summary()
+
+epochs = 500
+history = model.fit(padded_sequences, np.array(training_labels), epochs = epochs)
+
+#save the trained model
+model.save('chat-model')
+
+#save the fitted tokenizer
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol = pickle.HIGHEST_PROTOCOL)
+
+#save the fitted label encoder
+with open('label_encoder.pickle', 'wb') as ecn_file:
+    pickle.dump(lbl_encoder, ecn_file, protocol = pickle.HIGHEST_PROTOCOL)
 
 #model2
 # model = Sequential()
@@ -74,23 +94,4 @@ model.add(Dense(num_classes, activation='softmax'))
     # model.add(Dense(16, activation='relu'))
     # model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss='sparse_categorical_crossentropy', 
-              optimizer='adam', metrics=['accuracy']) 
-            #   'cosine_similarity', 'kullback_leibler_divergence'])
-            #   , 'poisson'
-
-model.summary()
-
-epochs = 500
-history = model.fit(padded_sequences, np.array(training_labels), epochs = epochs)
-
-#save the trained model
-model.save('chat-model')
-
-#save the fitted tokenizer
-with open('tokenizer.pickle', 'wb') as handle:
-    pickle.dump(tokenizer, handle, protocol = pickle.HIGHEST_PROTOCOL)
-
-#save the fitted label encoder
-with open('label_encoder.pickle', 'wb') as ecn_file:
-    pickle.dump(lbl_encoder, ecn_file, protocol = pickle.HIGHEST_PROTOCOL)
+ #   'cosine_similarity', 'kullback_leibler_divergence']) #   , 'poisson'
